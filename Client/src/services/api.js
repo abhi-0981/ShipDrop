327de16da7aedd9578ccb470329f818a868eaa37
abhi-0981 <absingh0981@gmail.com>
2026-09-22 20:56:22 +0530

    chnage

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "parceldrop-backend.netlify.app/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 export default api;
