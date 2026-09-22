ed8e8e6d62c0b0699af4b99f453a4f37fd080ea5
abhi-0981 <absingh0981@gmail.com>
2026-09-22 21:53:00 +0530

    CHANGE

--- Server/config/db.js
+++ Server/config/db.js
@@ -8,21 +8,21 @@ require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-    ssl: {
+  ssl: {
     rejectUnauthorized: false,
   },
-});
 
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 // ======================================================
 // DATABASE CONNECTION TEST
 // ======================================================
